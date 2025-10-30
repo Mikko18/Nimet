03c601e1c0ac12cfb1f3ca240eb7c1768bf73ada
Mikko18 <mikkomakipaa18@gmail.com>
2025-10-30 16:12:17 +0200

    Initial commit

---
+++ src/NameGuessingGame.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NameGuessingGame {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String correctName = "Eric";
+        String guess;
+
+        System.out.println("Guess the name:");
+        while (true) {
+            guess = in.nextLine();
+
+            if (guess.equalsIgnoreCase(correctName)) {
+                System.out.println("Yes! Congratulations!");
+                break;
+            } else {
+                System.out.println("No. Try again:");
+ }
+        }
+
+        
+    }
+}
+
